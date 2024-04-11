a3d241b162628af343f03256c43e9575f2443f48
as1248 <88760762+as1248@users.noreply.github.com>
2024-04-11 16:24:02 +0900

    Create NotFound
    NotFound 페이지 추가

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./routes/Main";
 import Search from "./routes/Search";
+import NotFound from "./routes/NotFound";
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" exact element={<Main/>}/>
         <Route path="/:num" exact element={<Search/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/NotFound.js
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return 111;
+}
+
+export default NotFound;
