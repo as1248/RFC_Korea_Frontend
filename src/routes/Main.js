7ce87c855d0a06363cfaf10123868db002f950ba
as1248 <88760762+as1248@users.noreply.github.com>
2024-04-12 17:05:09 +0900

    Update Main
    입력 기능 수정

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -49,7 +49,7 @@ const Main = () => {
           <div>RFC 문서를 제공합니다.</div>
         </SubTitle>
         <SearchArea>
-          <TextField label="RFC 번호를 입력해주세요." variant="outlined" ref={number} onChange={numberHandler} onKeyDown={keyDownHandler} error={!numberValid} inputProps={{maxLength:4}} sx={{backgroundColor:'white', borderRadius:'5px'}}/>
+          <TextField label="RFC 번호를 입력해주세요." variant="outlined" ref={number} onChange={numberHandler} onKeyDown={keyDownHandler} error={!numberValid} sx={{backgroundColor:'white', borderRadius:'5px'}}/>
           <Button variant="contained" onClick={search}><FaSearch size={24}/></Button>
         </SearchArea>
       </MainContent>
