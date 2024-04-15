3070a7c58cb43ad70731e9694387fd9da27a525f
as1248 <88760762+as1248@users.noreply.github.com>
2024-04-15 14:11:38 +0900

    Update NotFound
    잘못된 url 접근 시 에러 페이지 수정

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -1,5 +1,44 @@
+import styled from 'styled-components';
+import logo from '../img/logoWhite1.png'
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
 const NotFound = () => {
-  return 111;
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Logo src={logo} />
+      <Content>
+        <Text>존재하지 않는 페이지입니다.</Text>
+        <Button variant="contained" onClick={()=>navigate('/')} sx={{width:'80%', fontSize:'1.5rem'}}>메인 페이지로 이동</Button>
+      </Content>
+    </Container>
+  );
 }
 
 export default NotFound;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #AAD7D9;
+  height: 100vh;
+`;
+
+const Logo = styled.img`
+  height: 25rem;
+  margin-right: 5rem;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 4rem;
+  color: white;
+`;
+
+const Text = styled.div`
+  margin-bottom: 5rem;
+`;
