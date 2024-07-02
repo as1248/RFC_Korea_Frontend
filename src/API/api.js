993f43d8a051eec145c59847f7c2b3994f68b7a0
as1248 <88760762+as1248@users.noreply.github.com>
2024-07-02 22:42:26 +0900

    Update api
    api 요청 기능 수정

--- src/API/api.js
+++ src/API/api.js
@@ -1,9 +1,32 @@
-const BASEURL = '54.180.241.56:80';
+const BASEURL = 'http://54.180.241.56:80';
 
-export const searchRFC = async(controller,number) => {
+async function toJSON(body) {
+  const reader = body.getReader(); // `ReadableStreamDefaultReader`
+  const decoder = new TextDecoder();
+  const chunks = [];
+
+  async function read() {
+    const { done, value } = await reader.read();
+
+    // all chunks have been read?
+    if (done) {
+      return JSON.parse(chunks.join(''));
+    }
+
+    const chunk = decoder.decode(value, { stream: true });
+    chunks.push(chunk);
+    return read(); // read the next chunk
+  }
+
+  return read();
+}
+
+export const searchRFC = async(controller,number, setHtml, setTranslatedHtml) => {
   try{
     const response = await fetch(`${BASEURL}/search?no=1945`,{signal:controller.signal});
-    console.log(number);
+    const data = await toJSON(response.body);
+    setHtml(data.original);
+    setTranslatedHtml(data.translated);
   }catch(error){
     console.log(error);
   }
